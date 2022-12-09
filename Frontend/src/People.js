a0a11077ed291f87ed95ed1fa02f66461e62d445
celegen20 <celegen20@itu.edu.tr>
2022-12-10 00:13:33 +0300

    Implemented working Multiple Page with adjustable Entry per page

--- Frontend/src/People.js
+++ Frontend/src/People.js
@@ -1,23 +1,24 @@
 import {Table} from "./Table";
 import {default as url} from "./backendurl";
 import {useEffect, useState} from "react";
-import {PageNumber} from "./PageNumber";
+import {PageManager} from "./PageManager";
 
 export function People(){
     let [response, setResponse] = useState({"data": [], "header": [], maxPageCount:0})
     let [page, setPage] = useState(1);
+    let [entryPerPage, setEntryPerPage] = useState(100);
 
     useEffect(() => {
         async function fetchAndWrite() {
-            let response = await fetch(url + "/getPeople").then((response) => response.json());
+            let response = await fetch(url + "/getPeople?pageNumber=" + page + "&rowPerPage=" + entryPerPage).then((response) => response.json());
             setResponse(response);
         }
         fetchAndWrite();
-    } , [])
+    } , [page, entryPerPage])
     return (
         <>
             <h1>People Table Page</h1>
-            <PageNumber page={page} pageCount={response.maxPageCount} setPage={setPage}/>
+            <PageManager page={page} pageCount={response.maxPageCount} setPage={setPage} entryPerPage={entryPerPage} setEntryPerPage={setEntryPerPage}/>
             <Table data={response.data} header={response.header}/>
         </>);
 }
