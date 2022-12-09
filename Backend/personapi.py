1c9f9e1c58a1233cf318b35c7bca6ccf7d359afb
celegen20 <celegen20@itu.edu.tr>
2022-12-09 03:46:59 +0300

    Implemented backend function to return all of the headers and the data

--- Backend/personapi.py
+++ Backend/personapi.py
@@ -2,9 +2,20 @@ from __main__ import app, db
 
 from flask import make_response
 import TableConverters.PersonTableConverter as PersonTableConverter
-@app.route("/test")
-def test():
-    return "test"
+
+
+@app.route('/getPeople')
+def hello():
+
+    results = {
+        "data": db.executeSQLQuery("SELECT * FROM PERSON").fetchall(),
+        "header": [i[1] for i in db.executeSQLQuery("PRAGMA table_info(PERSON)").fetchall()]
+    }
+    response = make_response(results)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
 
 if db.executeSQLQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='PERSON'").fetchall() == []:
     PersonTableConverter.createAndFillPeopleTable(db)
