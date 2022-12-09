cc9733fc185dd05b01e69962ca8e8780950002df
celegen20 <celegen20@itu.edu.tr>
2022-12-09 03:48:07 +0300

    Implemented the actual page which fetch's data from backend and sets it to response variable

--- Frontend/src/People.js
+++ Frontend/src/People.js
@@ -1,3 +1,17 @@
+import {Table} from "./Table";
+import {default as url} from "./backendurl";
+import {useEffect, useState} from "react";
+
 export function People(){
-    return (<h1>People Table Page</h1>);
+    let [response, setResponse] = useState({"data": [], "header": []})
+
+    useEffect(() => {
+        async function fetchAndWrite() {
+            let response = await fetch(url + "/getPeople").then((response) => response.json());
+            console.log(response);
+            setResponse(response);
+        }
+        fetchAndWrite();
+    } , [])
+    return (<><h1>People Table Page</h1><Table data={response.data} header={response.header}/></>);
 }
