c49afca6b2634efc4454630166c84e0b5c920f06
celegen20 <celegen20@itu.edu.tr>
2022-12-09 03:46:13 +0300

    If person table doesn't exist, code creates it and fills it with the entries from the csv file

--- Backend/personapi.py
+++ Backend/personapi.py
@@ -1,6 +1,10 @@
-from __main__ import app
+from __main__ import app, db
 
+from flask import make_response
+import TableConverters.PersonTableConverter as PersonTableConverter
 @app.route("/test")
 def test():
     return "test"
 
+if db.executeSQLQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='PERSON'").fetchall() == []:
+    PersonTableConverter.createAndFillPeopleTable(db)
