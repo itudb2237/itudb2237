f107f36f6732af9abea98127eb2f298defeb9299
saltuk <saltukraci@gmail.com>
2022-12-14 01:33:37 +0300

    insert query update for security, new selection queries are added into the database

--- CSVtoSQL_pbtable/pbtable_csv2sql.py
+++ CSVtoSQL_pbtable/pbtable_csv2sql.py
@@ -1,10 +1,9 @@
-# Import required modules
 import csv
 import pandas as pd
 import sqlite3 as dbapi2
 import os
 from valueMaps import ValueMappings, HeadMappings
-
+i = 0
 class Database:
     def __init__(self, dbfile):
         self.dbfile = dbfile  
@@ -13,17 +12,18 @@ class Database:
         return ValueMappings[HeadMappings[column]][data[column]]
         
     def add_pbdata_fromCSV(self, data):
+        global i
         with dbapi2.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            print(HeadMappings["PBPTYPE"])
-            print(ValueMappings[HeadMappings["PBPTYPE"]][data["PBPTYPE"]])
-            query = f"""INSERT INTO PBTYPE (CASE_NUMBER, VEHICLE_NUMBER, PERSON_NUMBER, PERSON_TYPE, CROSSWALK_PRESENT, SIDEWALK_PRESENT, SCHOOLZONE_PRESENT, MOTOR_MANEUVER) VALUES
-             ({data["ST_CASE"]}, {data["VEH_NO"]}, {data["PER_NO"]}, {self.accessMap("PBPTYPE", data)}, {self.accessMap("PBCWALK", data)}, {self.accessMap("PBSWALK", data)}, {self.accessMap("PBSZONE", data)}, {self.accessMap("MOTMAN", data)})"""
-            print(query)
-            # since all attributes are integer
-            cursor.execute(query)
+            # print(HeadMappings["PBPTYPE"])
+            # print(ValueMappings[HeadMappings["PBPTYPE"]][data["PBPTYPE"]])
+            query = """INSERT INTO PBTYPE (CASE_NUMBER, VEHICLE_NUMBER, PERSON_NUMBER, PERSON_TYPE, CROSSWALK_PRESENT, SIDEWALK_PRESENT, SCHOOLZONE_PRESENT, MOTOR_MANEUVER) VALUES(?,?,?,?,?,?,?,?)"""
+            values = (int(data["ST_CASE"]), int(data["VEH_NO"]), int(data["PER_NO"]), self.accessMap("PBPTYPE", data), self.accessMap("PBCWALK", data), self.accessMap("PBSWALK", data), self.accessMap("PBSZONE", data), self.accessMap("MOTMAN", data))
+            cursor.execute(query, values)
             connection.commit()
             pbkey = cursor.lastrowid
+
+            query = "SELECT"
         return pbkey
 
 
@@ -59,13 +59,15 @@ SIDEWALK_PRESENT CHAR(70) NULL,
 SCHOOLZONE_PRESENT CHAR(70) NULL,
 MOTOR_MANEUVER CHAR(70) NULL,
 
-PRIMARY KEY (CASE_NUMBER, VEHICLE_NUMBER, PERSON_NUMBER),\n"""+ boolConditions + "CHECK (MOTOR_MANEUVER == "+" OR MOTOR_MANEUVER == ".join([i for i in set(ValueMappings["MOTOR_MANEUVER"].values())])+"))"
+PRIMARY KEY (CASE_NUMBER, VEHICLE_NUMBER, PERSON_NUMBER),\n"""
+create_table += boolConditions
+create_table += "CHECK (MOTOR_MANEUVER == \""+"\" OR MOTOR_MANEUVER == \"".join([i for i in set(ValueMappings["MOTOR_MANEUVER"].values())])+"\"))"
 print(create_table)
 # create sql table
 cursor.execute(create_table)
 
 # csv file content to pandas data frame
-df = pd.read_csv("FARS2015NationalCSV_pbtype.csv")
+df = pd.read_csv("FARS2015PuertoRicoCSV_pbtype.csv")
 content = df[['ST_CASE', 'VEH_NO', 'PER_NO', 'PBPTYPE', 'PBCWALK', 'PBSWALK', 'PBSZONE', 'MOTMAN']]
 attribute_number = len(content.columns)
 tuple_number = len(content) 
@@ -75,6 +77,7 @@ for i in range(tuple_number):
     db.add_pbdata_fromCSV(content.iloc[i, :])
 
 # selection query
+# id = "\"Not a Pedestrian\" ORDER BY PERSON_NUMBER DESC"
 select_all = "SELECT * FROM PBTYPE"
 rows = cursor.execute(select_all).fetchall()
  
--- CSVtoSQL_pbtable/valueMaps.py
+++ CSVtoSQL_pbtable/valueMaps.py
@@ -10,32 +10,32 @@ HeadMappings = {
 "MOTMAN": "MOTOR_MANEUVER"}
 
 ValueMappings = {"PERSON_TYPE": {
-                     5: "\"Pedestrian\"",
-                     6: "\"Bicyclist\"",
-                     7: "\"Other Cyclist\"",
-                     8: "\"Person on Personal Conveyances\"",
+                     5: "Pedestrian",
+                     6: "Bicyclist",
+                     7: "Other Cyclist",
+                     8: "Person on Personal Conveyances",
                 },
                  "CROSSWALK_PRESENT": {
-                    0: "\"None Noted\"",
-                    1: "\"Yes\"",
-                    9: "\"Unknown\""
+                    0: "None Noted",
+                    1: "Yes",
+                    9: "Unknown"
                  },
                  "SIDEWALK_PRESENT": {
-                    0: "\"None Noted\"",
-                    1: "\"Yes\"",
-                    9: "\"Unknown\""
+                    0: "None Noted",
+                    1: "Yes",
+                    9: "Unknown"
                  },
                  "SCHOOLZONE_PRESENT": {
-                    0: "\"None Noted\"",
-                    1: "\"Yes\"",
-                    9: "\"Unknown\""
+                    0: "None Noted",
+                    1: "Yes",
+                    9: "Unknown"
                  },
                  "MOTOR_MANEUVER": {
-                    1: "\"Left Turn\"",
-                    2: "\"Right Turn\"",
-                    3: "\"Straight Through\"",
-                    7: "\"Not a Pedestrian\"",
-                    8: "\"Not Applicable\"",
-                    9: "\"Unknown Motorist Maneuver\""
+                    1: "Left Turn",
+                    2: "Right Turn",
+                    3: "Straight Through",
+                    7: "Not a Pedestrian",
+                    8: "Not Applicable",
+                    9: "Unknown Motorist Maneuver"
                  }
                  }
