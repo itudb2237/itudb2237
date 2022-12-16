d73dd54204e1fbe5f808da18e7a769787dc95cd8
saltuk <saltukraci@gmail.com>
2022-12-16 20:36:13 +0300

    filter query is implemented

--- sql-queries/main.py
+++ sql-queries/main.py
@@ -31,7 +31,36 @@ class Database:
             cursor = connection.cursor()
             return cursor.execute("SELECT * FROM PBTYPE WHERE PERSON_TYPE = \"Bicyclist\"").fetchall()
 
-    
+    def getFilteredTable(self, formInput): # formInput is a dictionary, keys are columns, values are lists
+        statement = "SELECT "
+        
+        prevKey = ""
+        for key in formInput.keys():
+            if prevKey != "":
+                statement += prevKey + ", "
+            prevKey = key
+        statement += prevKey + " FROM PBTYPE WHERE "
+
+        last_key = list(formInput)[-1]
+        for key in formInput.keys():
+            if formInput[key] == None or len(formInput[key]) == 0:
+                statement += "( " + key + " IS NULL)" 
+            
+            else:
+                last_value = formInput[key][-1]
+                statement += "("
+                prevValue = ""
+                for value in formInput[key]:
+                    statement += key + " == \"" + value + "\""
+                    if value != last_value:
+                        statement += " OR "
+                statement += ")"
+            if key != last_key:
+                statement += " AND "
+        
+        print(statement)
+        return statement
+
     def update_pbdata(self, st_case, eventnum,):
         with dbapi2.connect(self.dbfile) as connection:
             pass
@@ -40,3 +69,14 @@ class Database:
 mydb = Database('pbtable.db')
 # mydb.getByPrimKey(430219,0,1)
 print(mydb.getPedestrians())
+formInput = {
+"CASE_NUMBER": [],
+"VEHICLE_NUMBER": [],
+"PERSON_NUMBER": [],
+"PERSON_TYPE": ["Pedesterian"],
+"CROSSWALK_PRESENT": ["Yes", "None"],
+"SIDEWALK_PRESENT": ["None"],
+"SCHOOLZONE_PRESENT": ["Yes", "None"],
+"MOTOR_MANEUVER": ["Right"],
+}
+mydb.getFilteredTable(formInput)
