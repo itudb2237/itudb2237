ada7a6b69b9683e5e6fe2f64ccc38d1b245bd559
saltuk <saltukraci@gmail.com>
2022-12-16 17:08:22 +0300

    Pbtypes table frontend is added

--- Frontend/src/Pbtypes.js
+++ Frontend/src/Pbtypes.js
@@ -1,3 +1,24 @@
+import {Table} from "./Table";
+import {default as url} from "./backendurl";
+import {useEffect, useState} from "react";
+import {PageManager} from "./PageManager";
+
 export function Pbtypes(){
-    return (<h1>Pbtypes Table Page</h1>);
+    let [response, setResponse] = useState({"data": [], "header": [], maxPageCount:0})
+    let [page, setPage] = useState(1);
+    let [entryPerPage, setEntryPerPage] = useState(100);
+
+    useEffect(() => {
+        async function fetchAndWrite() {
+            let response = await fetch(url + "/getPbtypes?pageNumber=" + page + "&rowPerPage=" + entryPerPage).then((response) => response.json());
+            setResponse(response);
+        }
+        fetchAndWrite();
+    } , [page, entryPerPage])
+    return (
+        <>
+            <h1>Pbtypes Table Page</h1>
+            <PageManager page={page} pageCount={response.maxPageCount} setPage={setPage} entryPerPage={entryPerPage} setEntryPerPage={setEntryPerPage}/>
+            <Table data={response.data} header={response.header}/>
+        </>);
 }
