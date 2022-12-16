8bfbc2c79cd2fe07d32c1952cfa2f7f2f32449c3
celegen20 <celegen20@itu.edu.tr>
2022-12-16 15:34:06 +0300

    Overlay page trigger is now assigned to specified value upon close and fixed content not showing up

--- Frontend/src/OverlayPage.js
+++ Frontend/src/OverlayPage.js
@@ -1,10 +1,10 @@
-export function OverlayPage({setTrigger}){
+export function OverlayPage({setTrigger, triggerNewValue, children}){
 
     return (
         <div style={{width: "100%", height: "100%", position: "absolute", top: "0px", left: "0px", zIndex: "1"}}>
-            <div style={{width: "50%", height: "100%", backgroundColor: "rgba(0,0,0,0.5)", top: "0px", left: "0px"}} onClick={()=>setTrigger(false)}/>
+            <div style={{width: "50%", height: "100%", backgroundColor: "rgba(0,0,0,0.5)", top: "0px", left: "0px"}} onClick={()=>setTrigger(triggerNewValue)}/>
             <div style={{width: "50%", height: "100%", backgroundColor: "rgb(0, 150, 0)", top:"0px", right: "0px", position: "absolute"}}>
-                {props.children}
+                {children}
             </div>
         </div>
     );
