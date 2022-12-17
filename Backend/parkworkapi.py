db1e9ebd07fcb8ad6148fcce0ef1dbccca25ff8c
bingol19 <bingol19@itu.edu.tr>
2022-12-17 15:30:45 +0300

    some changes on frontend of parkwork page

--- Backend/parkworkapi.py
+++ Backend/parkworkapi.py
@@ -3,6 +3,28 @@ from __main__ import app, db
 import flask
 import TableConverters.parkworkTableConverter as parkworkTableConverter
 
+
+parkwork_attributes = []
+query_result = db.executeSQLQuery("PRAGMA table_info(PARKWORK)").fetchall()
+
+for row in query_result:
+    name = row[1]
+    data_type = row[2]
+    
+    if data_type.startswith("CHAR"):
+        data_type = "CHAR"
+        
+    attribute = {"name": name, "type": data_type}
+    parkwork_attributes.append(attribute)
+
+@app.route('/getParkworkHeader', methods=['GET'])
+def getHeadersParkwork():
+    response = flask.make_response(parkwork_attributes)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
 @app.route('/getParkworks')
 def getParkworks():
     num_of_row_per_page = flask.request.args.get('num_of_row_per_page', default=100, type=int)
--- Frontend/src/Parkworks.js
+++ Frontend/src/Parkworks.js
@@ -1,8 +1,12 @@
 import {Table} from "./Table";
+import {TableForParkwork} from "./TableForParkwork";
 import {default as url} from "./backendurl";
 import {useEffect, useState} from "react";
-import {PageManager} from "./PageManager";
+//import {PageManager} from "./PageManager";
+//import {TableManagerForParkwork} from "./TableManagerForParkwork";
+import {TableManager} from "./TableManager";
 
+/*
 export function Parkworks(){
     let [response, setResponse] = useState({"data": [], "header": [], maxPageCount:0})
     let [page, setPage] = useState(1);
@@ -21,4 +25,65 @@ export function Parkworks(){
             <PageManager page={page} pageCount={response.maxPageCount} setPage={setPage} entryPerPage={entryPerPage} setEntryPerPage={setEntryPerPage}/>
             <Table data={response.data} header={response.header}/>
         </>);
+}
+*/
+
+async function fetchAndWrite(setValue, requestUrl) {
+	let resp = await fetch(requestUrl).then((response) => response.json());
+	setValue(resp);
+	return resp;
+}
+
+export function Parkworks(){
+	// Variable declarations
+	let [page, setPage] = useState(1);
+	let [order, setOrder] = useState("AZB");
+	let [columns, setColumns] = useState([{}]);
+	let [entryPerPage, setEntryPerPage] = useState(100);
+	let [orderBy, setOrderBy] = useState("CASE_NUMBER");
+	let [requestedColumns, setRequestedColumns] = useState([]);
+	let [response, setResponse] = useState({"data": [], maxPageCount:0})
+
+	// Runs once on page load
+	useEffect(() => {
+		fetchAndWrite(setColumns, url + "/getParkworkHeader").then((resp) => {
+			setRequestedColumns(resp);
+		})
+	}, [])
+
+	// Runs when page, entryPerPage, or requestedColumns changes (i.e. when the table needs to be updated) or when the page is first loaded
+	useEffect(() => {
+		let filters = (requestedColumns.filter(a => a["filter"] != null && (a["type"] != "INTEGER" || a["filter"].every(b => b != ""))).map(a => {
+			if(a["type"] == "INTEGER"){
+				return "filter" + a["name"] + "=" + a["filter"].join(",");
+			}
+			return "filter" + a["name"] + "=" + a["filter"] })).join("&")
+		fetchAndWrite(setResponse, url + "/getParkworks?pageNumber=" + page + "&rowPerPage=" + entryPerPage +
+			(requestedColumns.length != 0 ? "&requestedColumns=" + requestedColumns.map(a => a["name"]).join(",") : "")
+			+ (filters.length > 0 ? "&" + filters : "") + "&orderBy=" + orderBy + "&order=" + order)
+	} , [page, entryPerPage, requestedColumns, order, orderBy])
+	
+    return (
+		<>
+			<h1>Parkwork Table Page</h1>
+			<TableManager
+				page={page}
+				pageCount={response.maxPageCount}
+				setPage={setPage}
+				entryPerPage={entryPerPage}
+				setEntryPerPage={setEntryPerPage}
+				allColumns={columns}
+				requestedColumns={requestedColumns}
+				setRequestedColumns={setRequestedColumns}
+				orderBy={orderBy}
+				setOrderBy={setOrderBy}
+				order={order}
+				setOrder={setOrder}
+			/>
+			<TableForParkwork
+				header={requestedColumns}
+				setHeader={setRequestedColumns}
+				data={response.data}
+			/>
+		</>);
 }
