e5cefe2e2d0d8ca333ccff6cf64d079cc865bff3
ituitis20-yilmazmuha20 <yilmazmuha20@itu.edu.tr>
2022-12-19 04:49:35 +0300

    UpdateVehicleOverlay function added

--- Backend/Vehicle.js
+++ Backend/Vehicle.js
@@ -44,3 +44,68 @@ export function AddVehicleOverlay(props) {
 		</OverlayPage>
 	);
 }
+
+export function UpdateVehicleOverlay(props) {
+	let [vehicle, setVehicle] = useState([]);
+
+	useEffect(() => {
+		fetchAndWrite(setVehicle, url + "/getVehicle/" + props.vehicleData[0] + "/" + props.vehicleData[1]);
+	}, [])
+
+	return (
+		<OverlayPage
+			setTrigger={props.setTrigger}
+			triggerNewValue={[]}
+		>
+			<form
+				style={{display: "flex",justifyContent: "space-between", flexDirection: "column", height: "100%"}}
+				action={url + "/updateVehicle"} method={"POST"}
+			>
+				{props.allColumns.map((column, index) => {
+					let isDisabled = ["CASE_NUMBER", "VEHICLE_NUMBER"].some((value) => value == column["name"]);
+					return (
+						<div key={column["name"]+"_input_div"}>
+							<label style={{display: "inline"}} key={column["name"]+"_label"}>{column["name"] + ": "}</label>
+							{column["type"] == "INTEGER" ?
+								<input type="number"
+									   name={column["name"]}
+									   style={{display: "inline"}}
+									   key={column["name"]+"_input"}
+									   defaultValue={vehicle[index]}
+									   readOnly={isDisabled}
+								/>
+								:
+								(column["possibleValues"] == null ?
+										<input
+											type="text"
+											name={column["name"]}
+											style={{display: "inline"}}
+											key={column["name"]+"_input"}
+											defaultValue={vehicle[index]}
+											readOnly={isDisabled}
+										/>
+										:
+										<select
+											name={column["name"]}
+											style={{display: "inline"}}
+											key={column["name"]+"_select"}
+											readOnly={isDisabled}
+										>
+											{["NULL", ...column["possibleValues"].slice(3)].map((value) => {
+												return <option value={value} key={value+"_option"} selected={vehicle[index] == value}>{value}</option>
+											})}
+										</select>
+								)
+							}
+						</div>
+					)
+				})}
+				<input type="submit" value="Submit" style={{position: "absolute", bottom: "50px", width: "100%"}}/>
+			</form>
+			<button style={{position: "absolute", bottom: "0px", width: "100%"}} onClick={() => {
+				fetch(url + "/deleteVehicle/" + props.vehicleData[0] + "/" + props.vehicleData[1], {method: "POST"});
+			}
+			}>Delete this vehicle</button>
+		</OverlayPage>
+	);
+}
